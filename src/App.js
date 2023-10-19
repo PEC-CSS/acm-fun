2c187544eec4d6430af1edb0d11724a00487d5a8
Atharva Markandey <markandeyatharva@gmail.com>
2023-10-19 20:34:18 +0530

    calculator files under /pages/activities/components added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import {Games} from "./pages/Games";
 import {Activities} from "./pages/Activities";
 import {activities, games} from "./data/content";
 
+import Wrapper from "./activities/components/Wrapper";
+import Screen from "./activities/components/Screen";
+import ButtonBox from './activities/components/ButtonBox'
+import Button from './activities/components/Button'
+import CalcProvider from "./activities/context/CalcContext";
+
 function App() {
     return (
         <div className="App">
@@ -30,6 +36,32 @@ function App() {
                 }
             </Routes>
         </div>
+    );
+}
+
+const btnValues = [
+    ["C", "+-", "%", "/"],
+    [7, 8, 9, "x"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "+"],
+    [0, ".", "="],
+  ];
+
+function Calculator(){
+    return (
+        <CalcProvider>
+          <Wrapper>
+            <Screen />
+            <ButtonBox>
+              {btnValues.flat().map((btn, i) => (
+                <Button
+                  value={btn}
+                  key={i}
+                />
+              ))}
+            </ButtonBox>
+          </Wrapper>
+        </CalcProvider>
     );
 }
 
