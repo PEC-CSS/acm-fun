a46f32bf7f9fa127619c1c3544c76f015138f4ae
Manjot Singh Oberoi <78747188+ken1000minus7@users.noreply.github.com>
2023-10-20 21:56:03 +0530

    Update Games.js

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -1,15 +1,20 @@
-import { games } from "../data/content";
-import { GameCard } from "../components/games/GameCard";
+import "../styles/pages/Games.css"
+import {games} from "../data/content";
+import {GameCard} from "../components/games/GameCard";
 
 export const Games = () => {
     return (
-        <div className="game-root">
-            <h1 className="game-title">Games</h1>
-            <div className="game-content">
-                {games.map((game) => {
-                    return <GameCard game={game} />;
-                })}
+        <div className="games-root">
+            <h1 className="games-title">Games</h1>
+            <div className="games-content">
+                {
+                    games.map(game => {
+                        return (
+                            <GameCard game={game} />
+                        )
+                    })
+                }
             </div>
         </div>
-    );
-};
+    )
+}
