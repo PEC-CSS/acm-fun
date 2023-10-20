7ca9320285348cc81659a67d569e8287ee4774f5
Sergei Iu <serg_omen@mail.ru>
2023-10-20 14:48:47 +0300

    Change Navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,40 +1,95 @@
-import {Link} from "react-router-dom";
+import { useEffect } from 'react';
+import { Link, useLocation } from "react-router-dom";
 import "../../styles/components/common/Navbar.css";
 import logo from '../../logo.svg';
+import home_icon from '../../assets/icons/home-outline.svg';
+import game_icon from '../../assets/icons/game-controller-outline.svg';
+import pulse_icon from '../../assets/icons/pulse-outline.svg';
 
 const navbarOptions = [
     {
+        icon: home_icon,
         title: "Home",
         url: "/"
     },
     {
+        icon: game_icon,
         title: "Games",
         url: "/games"
     },
     {
+        icon: pulse_icon,
         title: "Activities",
         url: "/activities"
     }
 ]
 
 export const Navbar = () => {
+    let location = useLocation();
+
+    useEffect(() => {
+        const list = document.querySelectorAll('.list');
+   
+        if (location.pathname === "/") {
+            list[0].classList.add("active");
+            list[1].classList.remove("active");
+            list[2].classList.remove("active");
+        }
+
+        if (location.pathname === "/games") {
+            list[0].classList.remove("active");
+            list[1].classList.add("active");
+            list[2].classList.remove("active");
+        }
+
+        if (location.pathname === "/activities") {
+            list[0].classList.remove("active");
+            list[1].classList.remove("active");
+            list[2].classList.add("active");
+        }
+
+        function handleClick() {
+            list.forEach((item) => item.classList.remove("active"));
+            this.classList.add("active")
+        }
+        list.forEach((item) =>
+            item.addEventListener("click", handleClick));
+
+        console.log('location', location)
+    }, [location])
+
     return (
         <div className="navbar-root">
             <div className="navbar-heading">
-                <img src={logo} />
+                <img src={logo} alt="logo" />
                 <h1>ACM FUN</h1>
             </div>
-            <div className="navbar-content">
+            <ul>
                 {
-                    navbarOptions.map(item => {
+                    navbarOptions.map((item, i) => {
+                        
                         return (
-                            <Link to={item.url} className="navbar-item">
-                                {item.title}
-                            </Link>
+                            <li 
+                                key={i}
+                                className="list "
+                            >
+                                <Link 
+                                    to={item.url} 
+                                    class="navbar-item" 
+                                >
+                                    <span class="icon">
+                                        <img src={item.icon} alt={item.title} />
+                                    </span>
+                                    <span class="text">
+                                        {item.title}
+                                    </span>
+                                </Link>
+                            </li>
                         )
                     })
                 }
-            </div>
+            <   div class="indicator"></div>
+            </ul>
         </div>
     )
 }
