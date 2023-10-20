7611a81361bd6ae631b0b4a441887601dc3666f4
Manjot Singh Oberoi <78747188+ken1000minus7@users.noreply.github.com>
2023-10-20 21:57:15 +0530

    Update Activities.js

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -1,15 +1,20 @@
-import { activities } from "../data/content";
-import { ActivityCard } from "../components/activities/ActivityCard";
+import "../styles/pages/Activities.css"
+import {activities} from "../data/content";
+import {ActivityCard} from "../components/activities/ActivityCard";
 
 export const Activities = () => {
-  return (
-    <div className="activities-root">
-      <h1 className="activities-title">Activities</h1>
-      <div className="activities-content">
-        {activities.map((activity) => {
-          return <ActivityCard activity={activity} />;
-        })}
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className="activities-root">
+            <h1 className="activities-title">Activities</h1>
+            <div className="activities-content">
+                {
+                    activities.map(activity => {
+                        return (
+                            <ActivityCard activity={activity} />
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
