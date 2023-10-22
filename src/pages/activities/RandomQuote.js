30a6e24883bff627ac3b98655e97105f626493b3
Niloy <niloyman01@gmail.com>
2023-10-22 22:59:12 +0600

    Arceen: Improve UI of Random quote activity

--- src/pages/activities/RandomQuote.js
+++ src/pages/activities/RandomQuote.js
@@ -1,38 +1,44 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import "../../styles/pages/activities/RandomQuote.css"
+import "../../styles/pages/activities/RandomQuote.css";
 
 export const RandomQuote = () => {
+  const [quote, setQuote] = useState(null);
 
-    const [quote, setQuote] = useState()
+  const generateQuote = () => {
+    setQuote(null);
+    axios({
+      method: "GET",
+      url: "https://api.quotable.io/random",
+    })
+      .then((res) => setQuote(res.data))
+      .catch((error) => console.error(error));
+  };
 
-    const generateQuote = () => {
-        axios({
-            method: "GET",
-            url: "https://api.quotable.io/random"
-        }).then(res => setQuote(res.data))
-            .catch(error => console.error(error))
-    }
+  useEffect(() => {
+    generateQuote();
+  }, []);
 
-    useEffect(() => {
-        generateQuote()
-    }, [])
-
-    return (
-        <div className="rquote-root">
-            <h1>Random Quote Generator</h1>
-            <div>Generate any random quote to get some inspiration!</div>
-            {
-                quote && (
-                    <div className="rquote-content">
-                        <div className="rquote-quote">{quote.content}</div>
-                        <div className="rquote-author">- {quote.author}</div>
-                    </div>
-                )
-            }
-            <button className="rquote-button" onClick={generateQuote}>
-                Generate Quote
-            </button>
+  return (
+    <div className="rquote-root">
+      <h1 class="header">Random Quote Generator</h1>
+      <div class="description">
+        Generate any random quote to get some inspiration!
+      </div>
+      {quote && (
+        <div className="rquote-content">
+          <div className="rquote-quote">{quote.content}</div>
+          <div className="rquote-author">- {quote.author}</div>
         </div>
-    )
-}
+      )}
+      {!quote && (
+        <div className="spinner-wrapper">
+          <div className="spinner"></div>
+        </div>
+      )}
+      <button className="rquote-button" onClick={generateQuote}>
+        Generate Quote
+      </button>
+    </div>
+  );
+};
