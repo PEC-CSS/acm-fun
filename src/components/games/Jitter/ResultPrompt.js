187532834c0fd377b41a2d94e973ef3471aa1f94
lakshyeahh <121057440+lakshyeahh@users.noreply.github.com>
2023-10-23 14:57:51 +0530

    Update ResultPrompt.js

--- src/components/games/Jitter/ResultPrompt.js
+++ src/components/games/Jitter/ResultPrompt.js
@@ -1,170 +1,131 @@
 import React, { useState, useEffect } from 'react';
-import "../../styles/pages/games/Jitter.css";
-import clickSound from '../../assets/games/jitter/clicksound.wav';
-import ResultPrompt from '../../components/games/Jitter/ResultPrompt';
 
-export const Jitter = () => {
-  const [score, setScore] = useState(0);
-  const [clicks, setClicks] = useState(0);
-  const [timerOptions] = useState([5, 10, 30]);
-  const [userSelectedTime, setUserSelectedTime] = useState(10); // User-selected time
-  const [timeLeft, setTimeLeft] = useState(10); // Time left (initially equal to user-selected time)
-  const [isGameActive, setIsGameActive] = useState(false);
-  const [isGameComplete, setIsGameComplete] = useState(false);
-  const [ripples, setRipples] = useState([]);
-  const audio = new Audio(clickSound);
-  const containerRef = React.createRef();
+import goblinSound from '../../../assets/games/jitter/goblinSound.mp3';
+import otherSound from '../../../assets/games/jitter/otherSound.mp3';
+import star1Sound from '../../../assets/games/jitter/star1Sound.mp3';
+import star2Sound from '../../../assets/games/jitter/star2Sound.mp3';
+import star3Sound from '../../../assets/games/jitter/star3Sound.mp3';
+import star4Sound from '../../../assets/games/jitter/star4Sound.mp3';
+
+
+const ResultPrompt = ({ clicks, chosenTime, score, onClose }) => {
+  const [playSound, setPlaySound] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const scoreRulebook = {
+    5: [0, 7, 20, 30, 40],  // Modified score requirements for 5 seconds
+    10: [0, 15, 30, 45, 60], // Modified score requirements for 10 seconds
+    30: [0, 40, 80, 120, 160], // Modified score requirements for 30 seconds
+  };
+  
+
+  const calculateStars = (score, chosenTime) => {
+    const clickRanges = scoreRulebook[chosenTime];
+    if (!clickRanges) {
+      return 0; // Default to zero stars if no rule found
+    }
+
+    for (let i = 1; i <= 5; i++) {
+      if (score >= clickRanges[i - 1] && score <= clickRanges[i]) {
+        return i;
+      }
+    }
+
+    return 5;
+  };
+
+  const calculateTitle = (filledStars) => {
+    const titles = [
+      {
+        title: "A GOBLIN 🧌!",
+        line: "You've got potential.",
+      },
+      {
+        title: "A WITCH 🧙🏻‍♀️!",
+        line: "You're brewing up some clicks.",
+      },
+      {
+        title: "A BARBARIAN 💪🏼!",
+        line: "You're getting strong.",
+      },
+      {
+        title: "A BOWLER 🎳!",
+        line: "You're rolling in clicks.",
+      },
+      {
+        title: "A P.E.K.K.A! 🦾",
+        line: "You're a clicking machine.",
+      },
+      {
+        title: "A MEGA KNIGHT ⚔️!",
+        line: "You're the king of clicks.",
+      },
+    ];
+
+    return titles[filledStars];
+  };
+
+  const filledStars = calculateStars(score, chosenTime);
+  const { title, line } = calculateTitle(filledStars);
+
+  const getStarSound = (filledStars) => {
+    switch (filledStars) {
+      case 1:
+        return star1Sound;
+      case 2:
+        return star2Sound;
+      case 3:
+        return star3Sound;
+      case 4:
+        return star4Sound;
+      case 5:
+        return otherSound;
+      default:
+        return goblinSound; // Default sound for zero stars
+    }
+  };
 
   useEffect(() => {
-    let interval;
-
-    if (isGameActive && timeLeft > 0) {
-      interval = setInterval(() => {
-        setTimeLeft((prevTime) => prevTime - 1); // Decrement timeLeft
-      }, 1000);
-    } else if (timeLeft === 0) {
-      clearInterval(interval);
-      handleGameComplete();
-      setClicks((prevClicks) => prevClicks + 1); // Increment clicks when the timer runs off
-    }
-
-    return () => clearInterval(interval);
-  }, [isGameActive, timeLeft]);
-
-  const handleClick = (event) => {
-    if (!isGameActive) {
-      setScore(0);
-      setClicks(0);
-      setIsGameActive(true);
-      containerRef.current.style.animationPlayState = 'running';
-      setTimeLeft(userSelectedTime); // Set timeLeft to the user-selected time
-    } else if (isGameActive && timeLeft > 0) {
-      setScore((prevScore) => prevScore + 1);
-      setClicks((prevClicks) => prevClicks + 1);
+    if (playSound) {
+      const sound = getStarSound(filledStars);
+      const audio = new Audio(sound);
       audio.play();
+      setPlaySound(false);
+
+      setTimeout(() => {
+        setIsVisible(true);
+      }, 100);
     }
+  }, [playSound, filledStars]);
 
-    const newRipple = {
-      top: `${event.clientY - event.target.offsetTop}px`,
-      left: `${event.clientX - event.target.offsetLeft}px`,
-    };
-
-    setRipples([...ripples, newRipple]);
-
-    setTimeout(() => {
-      setRipples((prevRipples) => prevRipples.slice(1));
-    }, 1000);
+  const renderStars = (filled, empty) => {
+    const stars = [];
+    for (let i = 0; i < filled; i++) {
+      stars.push('★');
+    }
+    for (let i = 0; i < empty; i++) {
+      stars.push('☆');
+    }
+    return stars.join('');
   };
-
-  const handleRestartGame = () => {
-    setScore(0);
-    setClicks(0);
-    setIsGameActive(false);
-    setIsGameComplete(false);
-    containerRef.current.style.animationPlayState = 'paused';
-    setTimeLeft(userSelectedTime); // Reset timeLeft to the user-selected time
-  };
-
-  const handleGameComplete = () => {
-    setIsGameComplete(true);
-    containerRef.current.style.animationPlayState = 'paused';
-  };
-
-  console.log("time1:", userSelectedTime);
-
-  const starTable = [
-    { stars: 0, title: 'A GOBLIN 🧌!', description: "You've got potential." },
-    { stars: 1, title: 'A WITCH 🧙🏻‍♀️!', description: "You're brewing up some clicks." },
-    { stars: 2, title: 'A BARBARIAN 💪🏼!', description: "You're getting strong." },
-    { stars: 3, title: 'A BOWLER 🎳!', description: "You're rolling in clicks." },
-    { stars: 4, title: 'A P.E.K.K.A! 🦾', description: "You're a clicking machine." },
-    { stars: 5, title: 'A MEGA KNIGHT ⚔️!', description: "You're the king of clicks." },
-  ];
+  console.log("filledStars:", filledStars);
+  console.log("title:", title);
+  console.log("line:", line);
 
   return (
-    <div className='sun'>
-    <div className="jitter-click-game">
-      <h1 className='title-game'>JITTER CLICK GAME</h1>
-      <h6 className="sub">Boink the button desperately before the timer ends..</h6>
-      <div className="game-container">
-        <div className="left-panel">
-          <p className='box'>{clicks}</p>
-          <p className='box2'>Time: {timeLeft}</p> {/* Display timeLeft */}
-        </div>
-        <div className="center-panel">
-          {isGameComplete && (
-            <ResultPrompt
-              clicks={clicks}
-              chosenTime={userSelectedTime} // Pass user-selected time
-              score={score}
-              onClose={() => {
-                setIsGameComplete(false);
-                handleRestartGame();
-              }}
-            />
-          )}
-          <div className="click-button-container" ref={containerRef}>
-            <button
-              onClick={handleClick}
-              className={`click-button ${isGameActive ? 'active' : ''}`}
-            >
-              boink me
-            </button>
-          </div>
-        </div>
-        <div className="right-panel">
-          {timeLeft === 0 ? (
-            <button onClick={handleRestartGame} className="restart-button">
-              Try Again
-            </button>
-          ) : (
-            <div className="timer-options">
-              <p></p>
-              {timerOptions.map((option) => (
-                <button
-                  key={option}
-                  onClick={() => {
-                    if (!isGameActive) {
-                      setUserSelectedTime(option); // Update the user-selected time
-                   // Update timeLeft to match the new user-selected time
-                    }
-                  }}
-                  className={`timer-option-button ${
-                    userSelectedTime === option ? 'selected' : ''
-                  }`}
-                  disabled={isGameActive}
-                >
-                  {option} s
-                </button>
-              ))}
-            </div>
-          )}
-        </div>
-      </div>
-     
-    </div>
-    <div>
-    <table className="star-table">
-        <thead>
-          <tr>
-            <th>STARS</th>
-            <th>TITLE</th>
-     
-          </tr>
-        </thead>
-        <tbody>
-          {starTable.map((starInfo) => (
-            <tr key={starInfo.stars}>
-              <td>{starInfo.stars}</td>
-              <td>{starInfo.title}</td>
-           
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <div className={`result-prompt ${isVisible ? 'visible' : ''}`}>
+      <span className="close-button" onClick={onClose}>
+        &#10006;
+      </span>
+      <h4 className='score'>🚀CHECK YOUR SCORE</h4>
+      <h1 className='title1'>{title}</h1>
+      <h6 className='line1'>{line}</h6>
+      <p className='clicks'>Clicks: {clicks}</p>
+      <p>Time: {chosenTime} seconds</p>
+      <p className='stars'>STARS: {renderStars(filledStars, 5 - filledStars)}</p>
+    
     </div>
   );
-}
+};
 
-
+export default ResultPrompt;
