e218cc5a81d6c3bb3bb96e4ca619baaac61e9872
Sanidhya <sani28sharma@gmail.com>
2023-10-23 16:11:07 +0530

    Wordle changes

---
+++ src/pages/activities/getDefinition.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import axios from "axios";
+import "../../styles/pages/activities/getDefinition.css";
+
+const WordContent = ({definition})=>{
+  if(definition){
+    return(
+      <div className="word-content">
+        {
+          definition?.map((def, index) => (
+            <div className="word-definition">
+              <div key={index}><em>Definition: </em>{def.definition}</div>
+              <div key={index}><em>Example: </em>{def.example}</div>
+            </div>
+            )
+          )
+        }
+        </div>
+    )
+  }
+  return (
+    <div className="word-content">
+      <div className="no-definition">Word not found!</div>
+    </div>
+  )
+}
+
+export const SearchWord = () => {
+  const [term, setTerm] = useState();
+  const [definition, setDefinition] = useState([]);
+
+  const generateDefinition = () => {
+    axios({
+      method: "GET",
+      url: "https://api.urbandictionary.com/v0/define",
+      params: {
+        term: term,
+      },
+    })
+      .then((res) => {
+        console.log(res.data)
+        const allDefinitions = res.data.list;
+        if (allDefinitions.length) {
+          setDefinition(allDefinitions);
+        } else {
+          setDefinition(null);
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const handleSearch = () => {
+    generateDefinition();
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      generateDefinition();
+    }
+  };
+
+  console.log(definition)
+  return (
+    <div className="rquote-root">
+      <h1>Your Virtual Dictionary</h1>
+      <div>Search a word to get the Meaning Instantly!</div>
+      <div><br></br></div>
+      <div className="search-bar">
+        <input
+          id="Search"
+          type="text"
+          placeholder="Enter a word"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          onKeyPress={handleKeyPress} 
+        />
+        <button onClick={handleSearch} className="word-button">Search</button>
+      </div>
+      <WordContent definition={definition}/>
+    </div>
+  );
+};
---
+++ src/pages/games/Wordle.js
@@ -0,0 +1,321 @@
+import "../../styles/pages/games/wordle.css";
+import React, { useState, createContext, useEffect, useContext, useCallback } from "react";
+import wordBank from "./wordle-bank.txt";
+
+export const AppContext = createContext();
+
+const Letter = ({ letterPos, attemptVal }) => {
+  const { board, setDisabledLetters, currAttempt, correctWord, setAlmostLetters, setCorrectLetters } =
+    useContext(AppContext);
+  const letter = board[attemptVal][letterPos];
+  const correct = correctWord.toUpperCase()[letterPos] === letter;
+  const almost =
+    !correct && letter !== "" && correctWord.toUpperCase().includes(letter);
+  const letterState =
+    currAttempt.attempt > attemptVal &&
+    (correct ? "correct" : almost ? "almost" : "error");
+
+  useEffect(() => {
+    if (letter !== "" && !correct && !almost) {
+
+      setDisabledLetters((prev) => [...prev, letter]);
+    }
+    if (letter !== "" && correct) {
+
+      setCorrectLetters((prev) => [...prev, letter]);
+    }
+    if (letter !== "" && !correct && almost) {
+
+      setAlmostLetters((prev) => [...prev, letter]);
+    }
+  }, [currAttempt.attempt]);
+  return (
+    <div className="letter" id={letterState.toString()}>
+      {letter}
+    </div>
+  );
+}
+
+const Board = () => {
+  return (
+    <div className="board">
+      {" "}
+      <div className="row">
+        <Letter letterPos={0} attemptVal={0} />
+        <Letter letterPos={1} attemptVal={0} />
+        <Letter letterPos={2} attemptVal={0} />
+        <Letter letterPos={3} attemptVal={0} />
+        <Letter letterPos={4} attemptVal={0} />
+      </div>
+      <div className="row">
+        <Letter letterPos={0} attemptVal={1} />
+        <Letter letterPos={1} attemptVal={1} />
+        <Letter letterPos={2} attemptVal={1} />
+        <Letter letterPos={3} attemptVal={1} />
+        <Letter letterPos={4} attemptVal={1} />
+      </div>
+      <div className="row">
+        <Letter letterPos={0} attemptVal={2} />
+        <Letter letterPos={1} attemptVal={2} />
+        <Letter letterPos={2} attemptVal={2} />
+        <Letter letterPos={3} attemptVal={2} />
+        <Letter letterPos={4} attemptVal={2} />
+      </div>
+      <div className="row">
+        <Letter letterPos={0} attemptVal={3} />
+        <Letter letterPos={1} attemptVal={3} />
+        <Letter letterPos={2} attemptVal={3} />
+        <Letter letterPos={3} attemptVal={3} />
+        <Letter letterPos={4} attemptVal={3} />
+      </div>
+      <div className="row">
+        <Letter letterPos={0} attemptVal={4} />
+        <Letter letterPos={1} attemptVal={4} />
+        <Letter letterPos={2} attemptVal={4} />
+        <Letter letterPos={3} attemptVal={4} />
+        <Letter letterPos={4} attemptVal={4} />
+      </div>
+      <div className="row">
+        <Letter letterPos={0} attemptVal={5} />
+        <Letter letterPos={1} attemptVal={5} />
+        <Letter letterPos={2} attemptVal={5} />
+        <Letter letterPos={3} attemptVal={5} />
+        <Letter letterPos={4} attemptVal={5} />
+      </div>
+    </div>
+  );
+}
+
+const generateWordSet = async () => {
+  let wordSet;
+  let todaysWord;
+  await fetch(wordBank)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArr = result.split("\n");
+      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)].trim(); // Remove trailing \r
+      wordSet = new Set(wordArr.map((word) => word.trim())); // Remove \r from all words
+    });
+
+  return { wordSet, todaysWord };
+};
+
+const Key = ({ keyVal, bigKey, disabled, almost, correct }) => {
+  const { gameOver, onSelectLetter, onDelete, onEnter } =
+    useContext(AppContext);
+
+  const selectLetter = () => {
+    if (gameOver.gameOver) return;
+    if (keyVal === "ENTER") {
+      onEnter();
+    } else if (keyVal === "DELETE") {
+      onDelete();
+    } else {
+      onSelectLetter(keyVal);
+    }
+  };
+  return (
+    <div
+      className="key"
+      id={bigKey ? "big" : correct ? "correct" : disabled ? "disabled" : almost ? "almost" : undefined}
+      onClick={selectLetter}
+    >
+      {keyVal}
+    </div>
+  );
+}
+
+const Keyboard = () => {
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+  const {
+    board,
+    disabledLetters,
+    currAttempt,
+    gameOver,
+    onSelectLetter,
+    onEnter,
+    onDelete,
+    almostLetters,
+    correctLetters
+  } = useContext(AppContext);
+
+  const handleKeyboard = useCallback(
+    (event) => {
+      if (gameOver.gameOver) return;
+      if (event.key === "Enter") {
+        onEnter();
+      } else if (event.key === "Backspace") {
+        onDelete();
+      } else {
+        keys1.forEach((key) => {
+          if (event.key.toLowerCase() === key.toLowerCase()) {
+            onSelectLetter(key);
+          }
+        });
+        keys2.forEach((key) => {
+          if (event.key.toLowerCase() === key.toLowerCase()) {
+            onSelectLetter(key);
+          }
+        });
+        keys3.forEach((key) => {
+          if (event.key.toLowerCase() === key.toLowerCase()) {
+            onSelectLetter(key);
+          }
+        });
+      }
+    },
+    [currAttempt]
+  );
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    };
+  }, [handleKeyboard]);
+
+  return (
+    <div className="keyboard" onKeyDown={handleKeyboard}>
+      <div className="line1">
+        {keys1.map((key) => {
+          return <Key key={key} keyVal={key} disabled={disabledLetters.includes(key)} almost={almostLetters.includes(key)} correct={correctLetters.includes(key)} />;
+        })}
+      </div>
+      <div className="line2">
+        {keys2.map((key) => {
+          return <Key key={key} keyVal={key} disabled={disabledLetters.includes(key)} almost={almostLetters.includes(key)} correct={correctLetters.includes(key)} />;
+        })}
+      </div>
+      <div className="line3">
+        <Key keyVal={"ENTER"} bigKey />
+        {keys3.map((key) => {
+          return <Key key={key} keyVal={key} disabled={disabledLetters.includes(key)} almost={almostLetters.includes(key)} correct={correctLetters.includes(key)} />;
+        })}
+        <Key keyVal={"DELETE"} bigKey />
+      </div>
+    </div>
+  );
+}
+
+export const Wordle=()=> {
+
+  const boardDefault = [
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ];
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letter: 0 });
+  const [wordSet, setWordSet] = useState(new Set());
+  const [correctWord, setCorrectWord] = useState("");
+  const [disabledLetters, setDisabledLetters] = useState([]);
+  const [almostLetters, setAlmostLetters] = useState([]);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [gameOver, setGameOver] = useState({
+    gameOver: false,
+    guessedWord: false,
+  });
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      console.log(words)
+      setWordSet(words.wordSet);
+      setCorrectWord(words.todaysWord);
+    });
+  }, []);
+
+  const onEnter = () => {
+    if (currAttempt.letter !== 5) return;
+
+    let currWord = "";
+    for (let i = 0; i < 5; i++) {
+      currWord += board[currAttempt.attempt][i];
+    }
+    if (wordSet.has(currWord.toLowerCase())) {
+      if (currWord === correctWord.toUpperCase()) {
+        setGameOver({ gameOver: true, guessedWord: true });
+        return;
+      }
+      else if (currAttempt.attempt === 5) {
+        setGameOver({ gameOver: true, guessedWord: false });
+        return;
+      }
+      else {
+        setCurrAttempt({ attempt: currAttempt.attempt + 1, letter: 0 });
+      }
+    } else {
+      alert("Word not found");
+    }
+
+  };
+
+  const onDelete = () => {
+    if (currAttempt.letter === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letter - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letter: currAttempt.letter - 1 });
+  };
+
+  const onSelectLetter = (key) => {
+    if (currAttempt.letter > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letter] = key;
+    setBoard(newBoard);
+    setCurrAttempt({
+      attempt: currAttempt.attempt,
+      letter: currAttempt.letter + 1,
+    });
+  };
+
+
+
+  return (
+    <div className="Wordle">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          currAttempt,
+          setCurrAttempt,
+          correctWord,
+          onSelectLetter,
+          onDelete,
+          onEnter,
+          setDisabledLetters,
+          disabledLetters,
+          gameOver,
+          correctLetters,
+          setCorrectLetters,
+          almostLetters,
+          setAlmostLetters
+        }}
+      >
+        <div className="game">
+          <Board />
+          {gameOver.gameOver ? <div className="gameOver">
+            <h3 className="wordleWhite">
+              {gameOver.guessedWord
+                ? "You Correctly Guessed the Wordle"
+                : "You Failed to Guess the Word"}
+            </h3>
+            <h1 className="wordleWhite">Correct Word: {correctWord}</h1>
+            {gameOver.guessedWord && (
+              <h3 className="wordleWhite">You guessed in {currAttempt.attempt + 1} attempts</h3>
+            )}
+          </div> : <Keyboard />}
+        </div>
+      </AppContext.Provider>
+    </div>
+  );
+}
+
